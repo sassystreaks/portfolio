b956f88a8b75c44fdf551f82aaf0d19e959d5efc
tarana shetty <taranashetty30@gmail.com>
2024-08-28 01:28:23 +0530

    Resolved merge conflict in README.md

---
+++ src/constants/index.js
@@ -0,0 +1,50 @@
+import project1 from "../constants/project/project1.png";
+import project2 from "../constants/project/project2.png"
+
+
+export const HERO_CONTENT = `Hey there! I’m Streaks, your friendly neighborhood front-end developer with a knack for turning ideas into visually stunning and super functional websites. My portfolio is a playground where creativity meets code, showcasing the cool stuff I've built along my web development journey.
+
+Take a spin through my projects and see how I mix clean, efficient code with thoughtful design to create experiences that truly pop. `;
+
+export const ABOUT_TEXT = `Hi! I’m Tarana S Shetty, a front-end developer who loves being in “React” mode. With a strong foundation in JavaScript, HTML5, and CSS3, I’m all about crafting clean, responsive web experiences.
+
+I hold a B.E. in Information Science from Dayananda Sagar College of Engineering, where I didn’t just learn to code—I learned to code smart. At Hypersage Technology, I turned challenges into opportunities, optimizing performance and enhancing user experiences.
+
+I’ve also led teams, organized events, and even orchestrated a hackathon or two. My leadership style is like debugging—clear, precise, and always aiming for the best solution.
+
+When I’m not coding, I’m exploring new tech trends or dreaming up ways to make the web a better place. Let’s connect if you’re looking for someone who codes with passion and a dash of fun!.`;
+
+export const EXPERIENCES = [
+  {
+    year: " Feb 2024 - June 2024",
+    role: "Software Developer Intern",
+    company: "Hypersage Technology",
+    description: `During my internship at Hypersage Technology, I worked extensively with React.js and Redux to optimize front-end components, improving development efficiency. I also conducted user testing and implemented UX/UI enhancements, contributing to a more intuitive and engaging user experience.`,
+    technologies: ["Javascript", "React.js", "Node.js", "Redux"],
+  },
+  
+];
+
+export const PROJECTS = [
+  {
+    title: "VOYAGE: A Social Media App",
+    image: project1,
+    description:
+      "A dynamic social media app built using the MERN stack, featuring real-time updates, user authentication, and seamless user interactions. The app offers a responsive design and a robust back-end, ensuring a smooth and engaging experience across all devices.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  
+  {
+    title: "Blogging Application",
+    image: project2,
+    description:
+      "A full-stack blog application developed with Node.js, Express, and MongoDB, allowing users to create, edit, and share posts effortlessly. The app includes a clean, minimalist UI with efficient database management, making content creation and navigation intuitive.",
+    technologies: ["Node.js","Javascript", "MongoDB", "Express"],
+  },
+];
+
+export const CONTACT = {
+  address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+12 4555 666 00 ",
+  email: "taranashetty30@gmail.com",
+};
